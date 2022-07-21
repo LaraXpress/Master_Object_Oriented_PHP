cc20fec8d0550bf29948e46dcd0e3787e03bfbdc
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-07-21 23:21:19 +0600

    Update register.php

--- Database Logic/register.php
+++ Database Logic/register.php
@@ -12,23 +12,23 @@
 	<div class="container">
 	    	<div class="row mt-4">
 	    		<div class="col-md-10 offset-md-1">	    		
-	    			<form method="post">
-	    			  <div class="form-group">
-					    <label for="exampleInputEmail1">Username</label>
-					    <input type="text" class="form-control" name="username">
-					  </div>	
-					  <div class="form-group">
-					    <label for="exampleInputEmail1">Email address</label>
-					    <input type="email" class="form-control" name="email">
-					  </div>
-					  <div class="form-group">
-					    <label for="exampleInputPassword1">Password</label>
-					    <input type="password" class="form-control" name="password">
-					  </div>					  
-					  <button type="submit" name="register" class="btn btn-primary">Submit</button>
-					</form>
+	    		     <form method="post">
+				 <div class="form-group">
+				    <label for="exampleInputEmail1">Username</label>
+				    <input type="text" class="form-control" name="username">
+				  </div>	
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Email address</label>
+				    <input type="email" class="form-control" name="email">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Password</label>
+				    <input type="password" class="form-control" name="password">
+				  </div>					  
+				  <button type="submit" name="register" class="btn btn-primary">Submit</button>
+			     </form>
 	    		</div>
-	    	</div>
+	    	  </div>
 	    </div>    
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>    
