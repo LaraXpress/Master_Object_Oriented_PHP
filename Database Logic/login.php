d9039c90d9059e18e52dc617e1e196cdfb540c92
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-07-21 23:22:15 +0600

    Login UI setup

--- Database Logic/login.php
+++ Database Logic/login.php
@@ -1,1 +1,32 @@
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>    
+	<div class="container">
+	    	<div class="row mt-4">
+	    		<div class="col-md-10 offset-md-1">
+	    			<form method="post">
+	    			  <div class="form-group">
+					    <label for="exampleInputEmail1">Username</label>
+					    <input type="text" class="form-control" name="username">
+					  </div>						  
+					  <div class="form-group">
+					    <label for="exampleInputPassword1">Password</label>
+					    <input type="password" class="form-control" name="password">
+					  </div>					  
+					  <button type="submit" name="login" class="btn btn-primary">Submit</button>
+					</form>
+	    		</div>
+	    	</div>
+	    </div>    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>    
+  </body>
+</html>
